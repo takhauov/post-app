dc88df4286569cc0f2fa8103839b4e7b26b4bfa3
Artur <takhauov@gmail.com>
2021-01-04 15:00:52 +0300

    Add search panel and post status filter. Without fail

--- src/сomponents/app/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import AppHeader from '../app-header/app-header';
-const App = () => {
-    return (
-    <h1>Hello world!</h1>
-    // <AppHeader/>
-    )
-}
-export default App;
