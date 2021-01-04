668481f494374733ea0c74935c4a89cf8663b8bf
Artur <takhauov@gmail.com>
2021-01-04 12:13:39 +0300

    First commit

---
+++ src/сomponents/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Takhauov Artur</h1>
+            <h2>5 записей, из них понравилось 0</h2>
+        </div>
+    )
+}
+export default AppHeader;
---
+++ src/сomponents/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import AppHeader from '../app-header/app-header';
+const App = () => {
+    return (
+    <h1>Hello world!</h1>
+    // <AppHeader/>
+    )
+}
+export default App;
