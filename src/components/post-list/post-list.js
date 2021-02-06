5ea476f9d9a993fe65f9a553d5c28d2a6391871e
Artur <takhauov@gmail.com>
2021-02-06 15:22:20 +0300

    Add reactstrap

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PostListItem from '../post-list-item';
+import { ListGroup} from 'reactstrap';
+
 import './post-list.css';
 const PostList = ({posts}) => {
     const elements = posts.map ((item) => {
@@ -12,9 +14,9 @@ const PostList = ({posts}) => {
         )
     })
     return (
-        <ul className = "app-list list-group">
+        <ListGroup className = "app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 };
 export default PostList;
