2b990d820f04f4477f22a38f0a343a34e5ee82d4
Artur <takhauov@gmail.com>
2021-02-06 16:29:57 +0300

    Add styled-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,14 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './app.css';
+import styled from 'styled-components';
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
+// const StyledAppBlock = styled (AppBlock)` изменённая копия
+//     background-color: grey;
+// `
 const App = () => {
     const data = [
         {label: 'Going to learn React', important: true, id: 'sdc'},
@@ -12,7 +20,7 @@ const App = () => {
         {label: 'I need a break...', important: false, id: 'lkn'}
     ];
     return (
-        <div className = "app">
+        <AppBlock>
             <AppHeader/>
             <div className = "search-panel d-flex">
                 <SearchPanel/>
@@ -20,7 +28,7 @@ const App = () => {
             </div>
             <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 export default App;
