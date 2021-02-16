2a5e3cbf3e43adebfd2b27c393289611eaf17270
Artur <takhauov@gmail.com>
2021-02-16 21:10:11 +0300

    Addevent on delete button

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './post-add-form.css';
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className = "bottom-panel d-flex">
+        <div className = "bottom-panel d-flex">
             <input
                 type = "text"
                 placeholder = "О чём вы думаете сейчас?"
@@ -10,9 +10,10 @@ const PostAddForm = () => {
             />
             <button
                 type="submit"
-                className="btn btn-outline-secondary">
+                className="btn btn-outline-secondary"
+                onClick={() => onAdd('Hello')}>
             Добавить</button>
-        </form>
+        </div>
     )
 }
 export default PostAddForm;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,12 +3,14 @@ import PostListItem from '../post-list-item';
 import { ListGroup} from 'reactstrap';
 
 import './post-list.css';
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
     const elements = posts.map ((item) => {
         const {id, ...itemProps} = item;
         return(
             <li key={id} className='list-group-item'>
-                <PostListItem {...itemProps}
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete (id)}
                 />
             </li>
         )
